bb536830807b79f17b0155a9e2db6f55b38e3917
Kyeonga Lee <klee.mcintosh@gmail.com>
2019-08-07 20:24:08 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,12 +1,6 @@
 def run_guessing_game
-  number = rand(6) + 1 
-  puts 
-  
-  
-  
-def run_guessing_game
-  rando = rand(6)+1
-  puts "Guess a number between 1 and 6:"
+  rando = rand(6) + 1 
+  puts "Guess a number between 1 and 6"
   input = gets.chomp
   if input == rando.to_s
     puts "You guessed the correct number!"
