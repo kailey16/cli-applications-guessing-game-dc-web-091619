81b9c6fa3eac4a44c365e1df6fc4cf5da22be42d
Kyeonga Lee <klee.mcintosh@gmail.com>
2019-08-07 20:22:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,2 +1,18 @@
 def run_guessing_game
   number = rand(6) + 1 
+  puts 
+  
+  
+  
+def run_guessing_game
+  rando = rand(6)+1
+  puts "Guess a number between 1 and 6:"
+  input = gets.chomp
+  if input == rando.to_s
+    puts "You guessed the correct number!"
+  elsif input.downcase == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{rando}."
+  end
+end
