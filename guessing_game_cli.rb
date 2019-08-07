73ea2d050bd8e98efefecea28db8195340b23e8f
Kyeonga Lee <klee.mcintosh@gmail.com>
2019-08-07 20:25:21 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,6 +7,6 @@ def run_guessing_game
   elsif input.downcase == "exit"
     puts "Goodbye!"
   else
-    puts "Sorry! The computer guessed #{rando}."
+    puts "Sorry! The computer guessed #{number}."
   end
 end
